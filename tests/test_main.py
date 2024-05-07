6550b658eeee40dd21219562442aced2ace3c1c7
liza <368051@edu.itmo.ru>
2024-05-07 21:11:19 +0300

    Added golden test...

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,13 @@
 from python.main import increment_numbers
 
+import pytest
 
-def test_increment_numbers() -> None:
-    input_data = [3, 2, 3]
-    expected_output = [4, 3, 4]
+
+@pytest.mark.golden_test("goldens/*.yml")
+def test_increment_numbers(golden):
+    input_data = golden["in_data"]
+    expected_output = golden["out_data"]
+
     result = increment_numbers(input_data)
-    assert result == expected_output, "The increment function should correctly increment each element by 1"
+
+    assert result == expected_output, "increment_numbers не работает как ожидается"
