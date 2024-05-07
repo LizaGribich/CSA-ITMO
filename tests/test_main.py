a2524866a54f558f9b94ad043130283d1e06f88a
liza <368051@edu.itmo.ru>
2024-05-07 21:16:25 +0300

    Changed CI and tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,10 @@
 from python.main import increment_numbers
-
+from typing import Any
 import pytest
 
 
 @pytest.mark.golden_test("goldens/*.yml")
-def test_increment_numbers(golden):
+def test_increment_numbers(golden: Any) -> None:
     input_data = golden["in_data"]
     expected_output = golden["out_data"]
 
